5974b3651ceefeb9b61badea5c43e4f7d6ab60ab
githubteacher <sepehrskater@yahoo.com>
2020-09-20 17:19:53 +0200

    werkt voor geen ene meter

--- app/src/main/java/com/example/firstapp/FirstFragment.java
+++ app/src/main/java/com/example/firstapp/FirstFragment.java
@@ -16,16 +16,32 @@ public class FirstFragment extends Fragment {
     TextView showCountTextView;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
-        // Get the count text view
-        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return fragmentFirstLayout;
+        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
+                NavHostFragment.findNavController(FirstFragment.this).navigate(action);
+            }
+        });
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), "Hello toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/firstapp/SecondFragment.java
+++ app/src/main/java/com/example/firstapp/SecondFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.Random;
 public class SecondFragment extends Fragment {
 
     @Override
@@ -22,6 +24,25 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //code to get the current count, get the string and format it with the count, and then set it for textview_header.
+        Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String countText = getString(R.string.random_heading, count);
+        TextView headerView = view.getRootView().findViewById(R.id.textview_header);
+        headerView.setText(countText);
+
+
+        //Get a random number between 0 and the count.
+        Random random = new java.util.Random();
+        Integer randomNumber = 0;
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1);
+        }
+
+
+        //Add code to convert that number into a string and set it as the text for textview_random.
+        TextView randomView = view.getRootView().findViewById(R.id.textview_random);
+        randomView.setText(randomNumber.toString());
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
